ad171e0d4d482d6b889ed22c213d258b9930fba7
RIADH0007 <larbi.riadh10@gmail.com>
2025-03-03 16:00:33 +0100

    Update User.js

--- User.js
+++ User.js
@@ -1,39 +1,30 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  phoneNumber: {
-    type: String,
-    required: true,
-    match: /^(00213|\+213|0)(5|6|7)[0-9]{8}$/
-  },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  wilaya: { 
-    type: String, 
-    required: true,
-    enum: [
-      'Adrar', 'Chlef', 'Laghouat', 'Oum El Bouaghi', 'Batna', 'Béjaïa', 'Biskra',
-      'Béchar', 'Blida', 'Bouira', 'Tamanrasset', 'Tébessa', 'Tlemcen', 'Tiaret',
-      'Tizi Ouzou', 'Alger', 'Djelfa', 'Jijel', 'Sétif', 'Saïda', 'Skikda',
-      'Sidi Bel Abbès', 'Annaba', 'Guelma', 'Constantine', 'Médéa', 'Mostaganem',
-      'MSila', 'Mascara', 'Ouargla', 'Oran', 'El Bayadh', 'Illizi', 'Bordj Bou Arréridj',
-      'Boumerdès', 'El Tarf', 'Tindouf', 'Tissemsilt', 'El Oued', 'Khenchela',
-      'Souk Ahras', 'Tipaza', 'Mila', 'Aïn Defla', 'Naâma', 'Aïn Témouchent',
-      'Ghardaïa', 'Relizane'
-    ]
-  },
-  createdAt: { type: Date, default: Date.now }
-});
+const mongoose = require("mongoose");
 
-// تشفير كلمة المرور قبل الحفظ
-userSchema.pre('save', async function(next) {
-  if (this.isModified('password')) {
-    this.password = await bcrypt.hash(this.password, 10);
+const UserSchema = new mongoose.Schema({
+  firstName: { type: String },
+  familyName: { type: String },
+  email: { type: String, unique: true, sparse: true },
+  phoneNumber: { type: String, unique: true, sparse: true },
+  password: { type: String, required:true }, 
+  googleId: { type: String }, 
+  createdAt: { type: Date, default: Date.now },
+  resetOTP: { type: String, default: null },
+  otpExpires: { type: Date, default: null }
+},
+{ timestamps: true }
+);
+
+
+
+
+
+UserSchema.pre("save", function (next) {
+  if (!this.password && !this.googleId) {
+    return next(new Error("Either password or googleId must be provided"));
   }
   next();
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
