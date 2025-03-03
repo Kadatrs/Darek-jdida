4008f1044b8f3c4ffb58ba733890878182637bd9
RIADH0007 <larbi.riadh10@gmail.com>
2025-03-03 16:15:32 +0100

    Update User.js

--- User.js
+++ User.js
@@ -1,27 +1,27 @@
-
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  firstName: { type: String },
-  familyName: { type: String },
-  email: { type: String, unique: true, sparse: true },
-  phoneNumber: { type: String, unique: true, sparse: true },
-  password: { type: String, required:true }, 
-  googleId: { type: String }, 
-  createdAt: { type: Date, default: Date.now },
-  resetOTP: { type: String, default: null },
-  otpExpires: { type: Date, default: null }
-},
-{ timestamps: true }
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    familyName: { type: String, required: true },
+    email: { type: String, unique: true, sparse: true },
+    phoneNumber: { type: String, unique: true, sparse: true }, 
+    password: { type: String, required: true },
+    googleId: { type: String, default: null },
+    createdAt: { type: Date, default: Date.now },
+    resetOTP: { type: String, default: null },
+    otpExpires: { type: Date, default: null }
+  },
+  { timestamps: true }
 );
 
 
-
-
-
-UserSchema.pre("save", function (next) {
-  if (!this.password && !this.googleId) {
-    return next(new Error("Either password or googleId must be provided"));
+UserSchema.pre("validate", function (next) {
+  if (!this.email && !this.phoneNumber) {
+    return next(new Error("Either email or phone number is required."));
+  }
+  if (this.email && this.phoneNumber) {
+    return next(new Error("You can only provide either email or phone number, not both."));
   }
   next();
 });
