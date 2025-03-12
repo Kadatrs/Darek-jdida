698f3db818ba7248e6c8354fd318bb7faac79eff
Kadatrs <116973752+Kadatrs@users.noreply.github.com>
2025-03-12 18:17:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,63 +1,3 @@
-// require("dotenv").config();
-// const express = require("express");
-// const connectDB = require("./config/db");
-// const session = require("express-session");
-// const passport = require("passport");
-// require("./config1/passport"); // Load passport configuration
-
-// const app = express();
-
-// // Debugging: Check environment variables
-// console.log(" *** MONGO_URI:", process.env.MONGO_URI);
-// console.log(" *** PORT:", process.env.PORT);
-// console.log(" *** GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
-
-// // Connect to MongoDB
-// connectDB();
-
-
-// // Middleware
-// app.use(express.json());
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET || "mysecret",
-//     resave: false,
-//     saveUninitialized: true,
-//   })
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // Root route
-// app.get("/", (req, res) => {
-//   res.send("Hello, World! This is the Darek app!!");
-// });
-
-
-// // API routes
-// app.use("/api/auth/worker", require("./routes/workerAuth"));
-// app.use("/api/auth/user", require("./routes/userAuth"));
-
-// //app.use("/api/auth", require("./routes/userAuth")); 
-
-// // ✅ Import Routes
-// app.use("/api/auth", require("./routes/userAuth")); // User Auth Routes
-// app.use("/api/auth", require("./routes/workerAuth")); // Worker Auth Routes
-
-
-// // Start the server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(` *** Server is running on http://localhost:${PORT}`);
-// });
-
-
-
-
-
-
-
-
 
 require("dotenv").config();
 const express = require("express");
@@ -104,6 +44,8 @@ app.get("/", (req, res) => {
 app.use("/api/auth/user", require("./routes/userAuth")); // User Auth Routes
 app.use("/api/auth/worker", require("./routes/workerAuth")); // Worker Auth Routes
 app.use("/api/profile", require("./routes/profileRoutes")); // Profile Routes
+app.use("/api/requests", require("./routes/serviceRequestRoutes")); // service request Routes
+
 
 // ✅ Start the server
 const PORT = process.env.PORT || 5000;
