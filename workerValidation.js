85bc14855e321f87afb5c9f10be800f7bd528ddb
Kadatrs <116973752+Kadatrs@users.noreply.github.com>
2025-03-12 18:20:17 +0100

    Update workerValidation.js

--- workerValidation.js
+++ workerValidation.js
@@ -1,6 +1,15 @@
+
 const { body, validationResult } = require("express-validator");
 
 exports.registerValidation = [
+  body("firstName")
+    .isLength({ min: 2 }).withMessage("First name must be at least 2 characters long")
+    .matches(/^[\p{L} ]+$/u).withMessage("First name must contain letters only"),
+
+  body("familyName")
+    .isLength({ min: 2 }).withMessage("Last name must be at least 2 characters long")
+    .matches(/^[\p{L} ]+$/u).withMessage("Last name must contain letters only"),
+
   body("email")
     .optional()
     .isEmail().withMessage("Please enter a valid email address")
@@ -9,9 +18,8 @@ exports.registerValidation = [
   body("phoneNumber")
     .optional()
     .matches(/^(00213|\+213|0)(5|6|7)[0-9]{8}$/)
-    .withMessage("Please enter a valid phone number"),
+    .withMessage("Please enter a valid Algerian phone number"),
 
-    
   body().custom((value, { req }) => {
     if (!req.body.email && !req.body.phoneNumber) {
       throw new Error("You must enter at least an email or phone number");
@@ -23,14 +31,32 @@ exports.registerValidation = [
     .isLength({ min: 6 }).withMessage("Password must be at least 6 characters long")
     .matches(/[0-9]/).withMessage("Password must contain a number")
     .matches(/[a-zA-Z]/).withMessage("Password must contain a letter"),
-  
-    body("firstName")
-    .matches(/^[\p{L} ]+$/u).withMessage("First name must contain letters only"),
 
-  body("familyName")
-    .matches(/^[\p{L} ]+$/u).withMessage("Last name must contain letters only"),
+  body("job_type")
+    .isString().withMessage("Job type must be a string")
+    .notEmpty().withMessage("Job type is required"),
+
+  body("experience_years")
+    .optional()
+    .isNumeric().withMessage("Experience years must be a number")
+    .isInt({ min: 0 }).withMessage("Experience years must be at least 0"),
+
+  body("profile_image_url")
+    .optional()
+    .isURL().withMessage("Profile image URL must be a valid URL"),
+
+  body("address.latitude")
+    .optional()
+    .isNumeric().withMessage("Latitude must be a number"),
+
+  body("address.longitude")
+    .optional()
+    .isNumeric().withMessage("Longitude must be a number"),
+
+  body("address.full_address")
+    .optional()
+    .isString().withMessage("Full address must be a string"),
 ];
-
 
 exports.loginValidation = [
   body("email")
@@ -43,13 +69,11 @@ exports.loginValidation = [
     .matches(/^(00213|\+213|0)(5|6|7)[0-9]{8}$/)
     .withMessage("Please enter a valid phone number"),
 
-
-    body("password")
+  body("password")
     .isLength({ min: 6 }).withMessage("Password must be at least 6 characters long")
     .matches(/[0-9]/).withMessage("Password must contain a number")
     .matches(/[a-zA-Z]/).withMessage("Password must contain a letter"),
 ];
-
 
 exports.handleValidationErrors = (req, res, next) => {
   const errors = validationResult(req);
